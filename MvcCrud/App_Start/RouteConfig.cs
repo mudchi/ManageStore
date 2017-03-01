27fd57f8f41a7e00dda4959fe692a7574f8f8f6a
mudchi <potato_mud@hotmail.com>
2017-03-02 00:36:57 +0700

    Test Git

--- MvcCrud/App_Start/RouteConfig.cs
+++ MvcCrud/App_Start/RouteConfig.cs
@@ -8,7 +8,7 @@ using System.Web.Routing;
 namespace MvcCrud
 {
     public class RouteConfig
-    {
+    { 
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
